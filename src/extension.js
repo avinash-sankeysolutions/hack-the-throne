5baa3d33baf980a0948dfdabf2234c3e91cee271
avinash-sankeysolutions <136419706+avinash-sankeysolutions@users.noreply.github.com>
2023-09-15 12:39:52 +0530

    Update extension.js

--- src/extension.js
+++ src/extension.js
@@ -1,9 +1,10 @@
 const vscode = require('vscode');
+
 function activate(context) {
-  let disposable = vscode.commands.registerCommand('extension.detectSensitiveInfo', () => {
-  });
+  let disposable = vscode.commands.registerCommand('extension.detectSensitiveInfo', () => {});
   context.subscriptions.push(disposable);
-  let customKeywords = [
+
+  const customKeywords = [
     "password",
     "secret",
     "api_key",
@@ -12,8 +13,17 @@ function activate(context) {
     "client_secret",
     "confidential",
     "jwt_secret",
-    "database_password"
+    "database_password",
+    "let var",
+    "let const",
+    "var const",
+    "var let const",
+    "var let",
+    "let const var",
+    "secretAccessKey",
+    "AccessKey"
   ];
+
   const sensitiveInfoDecorationType = vscode.window.createTextEditorDecorationType({
     backgroundColor: 'rgba(255, 0, 0, 0.3)',
   });
@@ -55,16 +65,30 @@ function activate(context) {
     const decorations = [];
 
     for (let i = 0; i < document.lineCount; i++) {
-      const line = document.lineAt(i);
+      const line = document.lineAt(i).text;
+      if (line.includes("-----BEGIN RSA PRIVATE KEY-----") && line.includes("-----END RSA PRIVATE KEY-----")) {
+        // Found a line with RSA key delimiters
+        const startIndex = line.indexOf("-----BEGIN RSA PRIVATE KEY-----");
+        const endIndex = line.indexOf("-----END RSA PRIVATE KEY-----") + "-----END RSA PRIVATE KEY-----".length;
+        const range = new vscode.Range(i, startIndex, i, endIndex);
+        decorations.push({ range, hoverMessage: 'RSA private key found' });
+      }
       for (const keyword of customKeywords) {
         const regex = new RegExp(keyword, 'gi');
         let match;
-        while ((match = regex.exec(line.text))) {
+        while ((match = regex.exec(line))) {
           const startIndex = match.index;
           const endIndex = startIndex + match[0].length;
           const range = new vscode.Range(i, startIndex, i, endIndex);
           decorations.push({ range, hoverMessage: `Sensitive info found: ${match[0]}` });
         }
+      }
+      if (line.includes("AKIA")) {
+        // Found "AKIA" in the line, indicating a potential AWS Access Key
+        const startIndex = line.indexOf("AKIA");
+        const endIndex = startIndex + 16; // AWS Access Key format is AKIA followed by 12 alphanumeric characters
+        const range = new vscode.Range(i, startIndex, i, endIndex);
+        decorations.push({ range, hoverMessage: 'AWS Access key found' });
       }
     }
 
@@ -74,6 +98,7 @@ function activate(context) {
   vscode.workspace.onDidOpenTextDocument(document => {
     suggestNamingConventionsForDocument(document);
   });
+
   function suggestNamingConventionsForDocument(document) {
     if (!document) {
       return;
@@ -106,5 +131,5 @@ function activate(context) {
     activeEditor.setDecorations(camelCaseSuggestionDecorationType, suggestions);
   }
 }
-exports.activate = activate;
 
+exports.activate = activate;
